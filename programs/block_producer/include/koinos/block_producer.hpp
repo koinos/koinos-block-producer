7b6a36f0cc3aa1bce81765ed0f977afd0aab0000
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2021-03-17 16:16:41 -0700

    Block production logic #4

--- programs/block_producer/include/koinos/block_producer.hpp
+++ programs/block_producer/include/koinos/block_producer.hpp
@@ -1,10 +1,14 @@
+#include <koinos/mq/client.hpp>
+
 #include <memory>
+
+namespace koinos {
 
 namespace detail{ struct block_producer_impl; }
 
 struct block_producer
 {
-   block_producer();
+   block_producer( std::shared_ptr< mq::client > );
    ~block_producer();
 
    void start();
@@ -13,3 +17,5 @@ struct block_producer
    private:
       std::unique_ptr< detail::block_producer_impl > _my;
 };
+
+} // koinos
