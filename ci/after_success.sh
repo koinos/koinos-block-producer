aa7bce206135e9cfc27f0e7b08910011d4b5a955
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2021-04-14 14:13:05 -0700

    Create correct tag

--- ci/after_success.sh
+++ ci/after_success.sh
@@ -7,6 +7,6 @@ if ! [[ -z $BUILD_DOCKER ]]; then
    fi
 
    echo "$DOCKER_PASSWORD" | docker login -u $DOCKER_USERNAME --password-stdin
-   docker tag koinos-block-producer koinos/block-producer:$TAG
+   docker tag koinos-block-producer koinos/koinos-block-producer:$TAG
    docker push koinos/koinos-block-producer:$TAG
 fi
