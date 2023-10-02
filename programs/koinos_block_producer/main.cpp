c1023daca93d3fcb0de7ad9305a08b59c9abd8cb
Steve Gerbino <steve@gerbino.co>
2023-10-02 17:47:43 -0400

    Add log-datetime option

--- programs/koinos_block_producer/main.cpp
+++ programs/koinos_block_producer/main.cpp
@@ -44,7 +44,9 @@
 #define LOG_DIR_OPTION                     "log-dir"
 #define LOG_DIR_DEFAULT                    ""
 #define LOG_COLOR_OPTION                   "log-color"
-#define LOG_COLOR_DEFAULT                  false
+#define LOG_COLOR_DEFAULT                  true
+#define LOG_DATETIME_OPTION                "log-datetime"
+#define LOG_DATETIME_DEFAULT               true
 #define INSTANCE_ID_OPTION                 "instance-id"
 #define ALGORITHM_OPTION                   "algorithm"
 #define JOBS_OPTION                        "jobs"
@@ -107,7 +109,8 @@ int main( int argc, char** argv )
          (PRODUCER_ADDRESS_OPTION          ",f", program_options::value< std::string >(), "The beneficiary address used during PoB production")
          (APPROVE_PROPOSALS_OPTION         ",k", program_options::value< std::vector< std::string > >()->multitoken(), "A list a proposal to approve when producing a block")
          (LOG_DIR_OPTION                       , program_options::value< std::string >(), "The logging directory")
-         (LOG_COLOR_OPTION                     , program_options::value< bool >(), "Log color toggle");
+         (LOG_COLOR_OPTION                     , program_options::value< bool >(), "Log color toggle")
+         (LOG_DATETIME_OPTION                  , program_options::value< bool >(), "Log datetime on console toggle");
 
       program_options::variables_map args;
       program_options::store( program_options::parse_command_line( argc, argv, options ), args );
@@ -151,6 +154,7 @@ int main( int argc, char** argv )
       auto log_level         = util::get_option< std::string >( LOG_LEVEL_OPTION, LOG_LEVEL_DEFAULT, args, block_producer_config, global_config );
       auto log_dir           = util::get_option< std::string >( LOG_DIR_OPTION, LOG_DIR_DEFAULT, args, block_producer_config, global_config );
       auto log_color         = util::get_option< bool >( LOG_COLOR_OPTION, LOG_COLOR_DEFAULT, args, block_producer_config, global_config );
+      auto log_datetime      = util::get_option< bool >( LOG_DATETIME_OPTION, LOG_DATETIME_DEFAULT, args, block_producer_config, global_config );
       auto instance_id       = util::get_option< std::string >( INSTANCE_ID_OPTION, util::random_alphanumeric( 5 ), args, block_producer_config, global_config );
       auto algorithm         = util::get_option< std::string >( ALGORITHM_OPTION, FEDERATED_ALGORITHM, args, block_producer_config, global_config );
       auto jobs              = util::get_option< uint64_t >( JOBS_OPTION, std::max( JOBS_DEFAULT, uint64_t( std::thread::hardware_concurrency() ) ), args, block_producer_config, global_config );
@@ -172,7 +176,7 @@ int main( int argc, char** argv )
             logdir_path = basedir / util::service::block_producer / *logdir_path;
       }
 
-      koinos::initialize_logging( util::service::block_producer, instance_id, log_level, logdir_path, log_color );
+      koinos::initialize_logging( util::service::block_producer, instance_id, log_level, logdir_path, log_color, log_datetime );
 
       LOG(info) << version_string();
 
